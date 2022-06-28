030b9ad16773dd0562b1c2d301fe2dc4cb64bccc
nicksandoval <sand8val@gmail.com>
2022-06-28 10:33:42 -0400

    added preview

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,1 +1,9 @@
-// nothing here //
+const imgElement = document.getElementById("show-image");
+const fileElement = document.getElementById("image");
+
+fileElement.addEventListener('change', showImage);
+
+function showImage(event) {
+    imgElement.src = URL.createObjectURL(event.target.files[0]);
+    imgElement.onload = ()=> URL.revokeObjectURL(imgElement.src);
+};
